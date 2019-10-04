cc99d9687861b029623a3194e9ad8df0a9fddf13
bratyslav <beginner.altavista@gmail.com>
2019-10-04 14:10:53 +0300

    save

--- src/script.js
+++ src/script.js
@@ -1,3 +1,35 @@
-const navLinks = ['Home', 'About us', 'Servises', 'Events', 'Gallery', 'Contacts'];
+const navLinks = [
+  {
+    title: 'Home',
+    active: true,
+  },
+  {
+    title: 'About us',
+    active: false,
+  },
+  {
+    title: 'Servises',
+    active: false,
+  },
+  {
+    title: 'Events',
+    active: false,
+  },
+  {
+    title: 'Gallery',
+    active: false,
+  },
+  {
+    title: 'Contacts',
+    active: false,
+  },
+];
 
 const navList = document.getElementById('navList');
+navList.innerHTML=`
+  ${navLinks.map(link => (
+    `<li class="${link.active ? '_active' : ''}">
+      <a>${link.title}<a/>
+    </li>`
+  ))}
+`.replace(/,/g, '');
